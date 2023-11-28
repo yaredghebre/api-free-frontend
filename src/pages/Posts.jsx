24d951015ac84d40c12c7816d081b06b33878c8b
yaredghebre <yared2391@gmail.com>
2023-11-28 14:19:00 +0100

    cards implemented

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -14,17 +14,30 @@ const Posts = () => {
         console.error("Something went wrong while fetching posts:", error);
       });
   }, []);
+
   return (
     <div className="bg-green-200 h-screen">
       <div className="w-full container mx-auto py-11">
-        <h1 className="text-center text-4xl font-bold">
-          THIS IS THE POSTS PAGE
-        </h1>
-        <div className="w-full h-screen bg-red-400">
+        <h1 className="text-center text-4xl font-bold">ALL POSTS</h1>
+        <div className="flex flex-wrap justify-center">
           {posts.map((post) => (
-            <div key={post.id}>
-              <h2>{post.title}</h2>
-              <p>{post.content}</p>
+            <div
+              key={post.id}
+              class="max-w-sm m-4 bg-white border border-gray-200 cursor-pointer transform transtion duration-150 hover:scale-105 hover:border-gray-400 hover:shadow-lg hover:shadow-gray-500 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            >
+              <a href="#">
+                <img class="rounded-t-lg" src={post.image} alt={post.title} />
+              </a>
+              <div class="p-5">
+                <a href="#">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {post.title}
+                  </h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {post.content}
+                </p>
+              </div>
             </div>
           ))}
         </div>
